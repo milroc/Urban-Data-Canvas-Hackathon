509c68c0dcdda33f7cc987dcc41f6604c235eaba
Sarah Groff-Palermo <sarah.groff.palermo@gmail.com>
2014-02-22 15:01:24 -0800

    Dark background

--- js/app.js
+++ js/app.js
@@ -10,5 +10,22 @@ $(document).ready(function(){
 
   $allButtons.click(function(){
     $(this).toggleClass('btn-highlight');
+  });
+
+  $embarcadero.click(function(){
+    $('button[name="embarcadero"] > .fa').toggleClass('hidden');
+  });
+
+  $montgomery.click(function(){
+    $('button[name="montgomery"] > .fa').toggleClass('hidden');
   })
+
+  $powell.click(function(){
+    $('button[name="powell"] > .fa').toggleClass('hidden');
+  })
+
+  $cc.click(function(){
+    $('button[name="cc"] > .fa').toggleClass('hidden');
+  })
+
 });
